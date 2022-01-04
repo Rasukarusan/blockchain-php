37b049e97860efefe2c7ab7c7d92f413aa089f3e
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-01-04 17:40:53 +0900

    ブロックチェーンクラスを作成

---
+++ Blockchain.php
@@ -0,0 +1,105 @@
+<?php
+
+class Blockchain
+{
+    private $chain;
+
+    private $current_transactions;
+
+    public function __construct()
+    {
+        $this->chain = [];
+        $this->current_transactions = [];
+    }
+
+    /**
+     * 新規ブロックの作成とチェーンに追加.
+     * @param int         $proof
+     * @param null|string $previous_hash
+     * @return array 新規ブロック
+     */
+    public function newBlock(int $proof, string $previous_hash = null): array
+    {
+        $block = [
+            'index' => count($this->chain) + 1,
+            'timestamp' => time(),
+            'transactions' => $this->current_transactions,
+            'proof' => $proof,
+            'previous_hash' => $this->createHash(self::lastBlock()),
+        ];
+
+        // トランザクションをリセット
+        $this->current_transactions = [];
+
+        $this->chain[] = block;
+        return $block;
+    }
+
+    /**
+     * トランザクションを追加.
+     *
+     * @param string $sender    送信者のアドレス
+     * @param string $recipient 受信者のアドレス
+     * @param int    $amount    量
+     * @return int 最後のブロックの次のインデックス
+     */
+    public function newTransaction(string $sender, string $recipient, int $amount): int
+    {
+        $this->current_transactions[] = [
+            'sender' => $sender,
+            'recipient' => $recipient,
+            'amount' => $amount,
+        ];
+        return self::lastBlock()['index'] + 1;
+    }
+
+    /**
+     * プルーフオブワーク.
+     *
+     * @param int $last_proof
+     * @return int
+     */
+    public function proofOfWork(int $last_proof): int
+    {
+        $proof = 0;
+        while (self::validProof($last_proof, $proof)) {
+            $proof++;
+        }
+        return $proof;
+    }
+
+    /**
+     * プルーフのバリデーション.
+     *
+     * @param int $last_proof
+     * @param int $proof
+     * @return bool true: 正、false: 誤
+     */
+    private static function validProof(int $last_proof, int $proof): bool
+    {
+        $guess = "{$last_proof}{$proof}";
+        $guess_hash = hash('sha256', $guess);
+        return substr($guess_hash, -4) === '0000';
+    }
+
+    /**
+     * ブロックのSHA-256ハッシュを生成.
+     *
+     * @param array $block
+     * @return string SHA-256ハッシュ
+     */
+    private static function createHash($block): sring
+    {
+        $block_string = json_encode($block);
+        return hash('sha256', $block_string);
+    }
+
+    /**
+     * チェーンの最後のブロックを返す.
+     * @return array 最後のブロック
+     */
+    private static function lastBlock(): array
+    {
+        return end($this->chain);
+    }
+}
